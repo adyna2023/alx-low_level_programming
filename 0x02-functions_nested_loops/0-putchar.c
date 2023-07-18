8e95384426cb00b2618f56aa59fdb7e38376f30d
adyna2023 <a.mawuyram21@gmail.com>
2023-07-18 16:51:05 -0700

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * main - print _putchar
- * is written by mbah
- * Return: 0
- */
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -18,3 +18,4 @@ _putchar('r');
 _putchar('\n');
 return (0);
 }
+
