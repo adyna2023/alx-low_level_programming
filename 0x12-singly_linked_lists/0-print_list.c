e241ca036f53caa75656d4f638c8ea4c6f797aa3
adyna2023 <a.mawuyram21@gmail.com>
2023-08-25 09:56:46 -0700

    printlist

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,26 +3,24 @@
 #include "lists.h"
 
 /**
- * print_list - prints and check the code
+ * print_list - to prints and checks the code
  * @h: name of the list_t list to print
- * Return: the number of nodes of h
+ * Return : the number of nodes of h
  */
-size_t print_list(const list_t *h);
+
+size_t print_list(const list_t *h)
 {
-	size_t size = 0;
+	size_t count = 0;
 
 	while (h)
 	{
-	if (h->str == NULL)
-	{
-	printf("[0] (nil)\n");
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
 	}
-	else
-	{
-		printf("[%zu] %s\n", h->len, h->str);
-	}
-	size++;
-	h = h->next;
-	}
-	return (size);
+
+	return (count);
 }
