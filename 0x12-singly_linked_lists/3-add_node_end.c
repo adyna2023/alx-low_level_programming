2a12b24989a60e53c77cc75a7200e7a4fa2ba69d
adyna2023 <a.mawuyram21@gmail.com>
2023-08-25 11:36:32 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: oringinal linked list
+ * @str: to add a new string to the node
+ *
+ * Return: the address of the new element, or NULL if it fails
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new;
+	list_t *current = *head;
+	unsigned int len = 0;
+
+	while (str[length])
+		length++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+
+	while (current->next)
+		current = current->next;
+
+	current->next = new;
+
+	return (new);
+}
+
