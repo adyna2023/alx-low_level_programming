021a04d16fff85f1c3bba1a987d6efe4443ae78a
adyna2023 <a.mawuyram21@gmail.com>
2023-08-29 17:41:00 -0700

    sum

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -10,12 +10,12 @@
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
-	listint_t *temp = head;
+	listint_t *temp_node = head;
 
-	while (temp)
+	while (temp_node)
 	{
-		sum += temp->n;
-		temp = temp->next;
+		sum += temp_node->n;
+		temp_node = temp_node->next;
 	}
 
 	return (sum);
