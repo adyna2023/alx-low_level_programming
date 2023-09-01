059a7881695f03514c7548dce25047670d3a7462
adyna2023 <a.mawuyram21@gmail.com>
2023-08-31 18:24:23 -0700

    new

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -13,11 +13,11 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	int i, count = 0;
 	unsigned long int current;
-	unsigned long int num = n ^ m;
+	unsigned long int exclusive = n ^ m;
 
 	for (i = 65; i >= 0; i--)
 	{
-		current = num >> i;
+		current = exclusive >> i;
 		if (current & 1)
 			count++;
 	}
